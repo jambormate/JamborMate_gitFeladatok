c73fe49d44a1b5de661f8f958517c3ea4c987624
jambormate <jmate2007@gmail.com>
2024-12-15 17:14:21 +0100

    F01 feladat elkészítve

--- Github kezdes/Program.cs
+++ Github kezdes/Program.cs
@@ -8,8 +8,18 @@ namespace Github_kezdes
 {
     internal class Program
     {
+        static void f01(int[] args)
+        {
+            Random random = new Random();
+            for (int i = 0; i < args.Length; i++)
+            {
+                args[i] = random.Next(1, 50);
+            }
+        }
         static void Main(string[] args)
         {
+            int[] args1 = new int[10];
+            f01(args1);
         }
     }
 }
