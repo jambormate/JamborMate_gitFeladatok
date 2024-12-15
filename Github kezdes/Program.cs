d05efe2e421c923904bebb1000661ddc2c94d015
jambormate <jmate2007@gmail.com>
2024-12-15 17:25:02 +0100

    F02 feladat elkészítve

--- Github kezdes/Program.cs
+++ Github kezdes/Program.cs
@@ -16,10 +16,23 @@ namespace Github_kezdes
                 args[i] = random.Next(1, 50);
             }
         }
+        static int f02(int[] args)
+        {
+            int szamlalo = 0;
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] %2 == 0) 
+                {  
+                    szamlalo++; 
+                }
+            }
+            return szamlalo;
+        }
         static void Main(string[] args)
         {
             int[] args1 = new int[10];
             f01(args1);
+            Console.WriteLine(f02(args1));
         }
     }
 }
