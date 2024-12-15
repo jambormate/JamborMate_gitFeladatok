e465217106bd0a20dfb5a54a8e9d6099aef55bb3
jambormate <jmate2007@gmail.com>
2024-12-15 17:32:23 +0100

    F03 feladat elkészítve

--- Github kezdes/Program.cs
+++ Github kezdes/Program.cs
@@ -28,11 +28,23 @@ namespace Github_kezdes
             }
             return szamlalo;
         }
+
+        static double f03(int[] args)
+        {
+            double atlag = 0;
+            for (int i = 0; i < args.Length; i++)
+            {
+                atlag += args[i]; 
+            }
+            return atlag/args.Length;
+        }
+
         static void Main(string[] args)
         {
             int[] args1 = new int[10];
             f01(args1);
             Console.WriteLine(f02(args1));
+            Console.WriteLine(f03(args1));
         }
     }
 }
